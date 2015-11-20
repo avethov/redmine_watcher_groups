f1d2fc8e9c776d988177441dbaf1e05e028ad25f
Petr Mlčoch <you@example.com>
2015-11-20 11:28:40 +0100

    fixed rendering for for new issue after change of tracker #4511

--- lib/watcher_groups_issues_controller_patch.rb
+++ lib/watcher_groups_issues_controller_patch.rb
@@ -6,6 +6,18 @@ module WatcherGroupsIssuesControllerPatch
         base.class_eval do
           helper :watcher_groups
           include WatcherGroupsHelper
+
+          def new
+            respond_to do |format|
+              format.html { 
+                render :action => 'new', :layout => !request.xhr? 
+              }
+              format.js {
+                #this is needed for proper rendering /projects/:project/issues/new.js
+                render :action => 'new', formats: [:js], :layout => false
+              }
+            end
+          end  
         end
     end
 
